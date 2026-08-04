38a568dea0c11fba5c4fe3fc5751f09a3cfd6d51
your_username <your_email@gmail.com>
2026-08-04 14:54:47 +0700

    Mô tả phần code vừa chỉnh sửa

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ let age = 19;
 const name = "Nguyễn Văn A";
 console.log(name);
 console.log(age);
+
+// Kiểu dữ lieu
+// 1. String (chuỗi) : "hoadv", "javascript", 'hoadv' 
+// 2. Number (số)
+// 3. Boôlean (đúng hoặc sai)
+let game = "Liên Quân";
+let year = 9;
+let isAdmin = true;
+console.log(game);
