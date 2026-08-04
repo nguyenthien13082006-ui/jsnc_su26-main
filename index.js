a8ed5afdafc79d90c63544c9ae09fecfe23c499a
your_username <your_email@gmail.com>
2026-08-04 14:42:24 +0700

    Mô tả phần code vừa chỉnh sửa

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('javascrip');
 
-let name = "Nguyễn Văn A";
-const age = 19;
-console.log('Nguyễn Văn A');
-
+let age = 19;
+const name = "Nguyễn Văn A";
+console.log(name);
+console.log(age);
