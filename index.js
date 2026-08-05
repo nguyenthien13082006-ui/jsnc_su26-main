439197b6e180317ccd7c21c8d89efb90141ff9ae
your_username <your_email@gmail.com>
2026-08-05 15:04:01 +0700

    Mô tả phần code vừa chỉnh sửa

--- index.js
+++ index.js
@@ -13,9 +13,23 @@ let game = "Liên Quân";
 let year = 9;
 let isAdmin = true;
 console.log(game);
-alert('Free Fire sống dai thành huyền thoại');
+
+// alert('Free Fire sống dai thành huyền thoại');
+
 const h1 = document.getElementById('title').innerHTML = "Danh sách vật phẩm";
 console.log(h1);
-const title = "danh sách vật phẩm"
-h1.innerHTML = "Danh sách vật phẩm";
-console.log(title);
+
+const user = {
+    id: 1,
+    name: 'Nguyễn Ngọc Thiện',
+    age: 19,
+    email: 'thien.nguyen@example.com',
+    address: 'Hà Nội'
+};
+
+// In toàn bộ thông tin ra Console
+console.log(user);
+console.log("Họ tên:", user.name);
+console.log("Tuổi:", user.age);
+console.log("Email:", user.email);
+console.log("Địa chỉ:", user.address);
